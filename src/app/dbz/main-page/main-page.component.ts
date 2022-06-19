a3cd480087107a06e387d978787331b8cd059b35
ivani2 <eltalivanitodos@gmail.com>
2022-06-19 14:06:44 -0500

    Shaketree 1 for 5.0

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
-import { DbzService } from '../services/dbz.service';
+
 
 @Component({
   selector: 'app-main-page',
--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Personaje } from '../interfaces/dbz.interface';
+import { Component } from '@angular/core';
 
 import { DbzService } from '../services/dbz.service';
 
